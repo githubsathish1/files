3a656d5efa0183c1df5e24a66ab448ec2ff2140e
chandu <sathish4d52gmail.com>
2019-04-02 00:28:36 -0700

    sas

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,7 @@
-provider "aws" { 
+  resource "aws_instance" "example" { 
+ami = "ami-2757f631" 
+instance_type = "t2.micro" 
+}provider "aws" { 
    access_key = "AKIAITQRVF2K5BO7ZPYA" 
    secret_key = "J76aA4QrK8RfsNTDpXHa9NgxgcRw5FGtGDshBV8G" 
    region = "us-east-1" 
